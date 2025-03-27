d7d8754c49d23a86022fed06747a4734d7382d09
Enamul Hasan <mhasan173013@bscse.uiu.ac.bd>
2025-03-27 16:00:52 +0600

    db connected

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import { connectDB } from './db.js'
+
+const app = express()
+
+const PORT = process.env.PORT || 4000
+
+try {
+    connectDB();
+    console.log("Coonect To Db!!!")
+} catch (error) {
+    console.error(err)
+    process.exit(1)
+}
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send('Welcome to Coders Bank!')
+})
+
+app.get('/hello',(req,res)=>{
+    res.send('Helloo!!!');
+})
+
+const reqLogger = (req,res,next)=>{
+    console.log(`${req.method} ${req.url} ${new Date().toISOString()}`);
+    next()
+}
+
+app.use(reqLogger)
+
+app.get('/health',(req,res)=>{
+    console.log(req.query.name)
+    res.status(200).json({"messege":'Hey!I am Healthy.this is not the same format'})
+})
+
+app.post('/api/users',reqLogger,(req,res)=>{
+    throw new Error ('Something went wrong!')
+    res.json({})
+})
+
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(500).json({message:'Something wrong! '})
+})
+
+app.listen(PORT,()=>console.log(`Server is running ${PORT}`))
